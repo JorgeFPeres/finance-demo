741a326ecc9f4bb8b22d2035f8e6a419e8e98cfd
JorgeFPeres <jorge-peres@live.com>
2025-04-03 23:56:23 -0300

    feat(auth): create register page using AuthForm

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,6 +1,6 @@
 import { AuthForm } from '@/components/shared/AuthForm'
 
-export default function LoginPage() {
+export default function RegisterPage() {
   return (
     <div className='flex min-h-screen items-center justify-center bg-gray-50 p-4'>
       <div className='bg-white shadow-xl rounded-xl p-8 w-full max-w-md'>
