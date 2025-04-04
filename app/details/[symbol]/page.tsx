476e03c92617ca57757f619c75e03e7963106c95
JorgeFPeres <jorge-peres@live.com>
2025-04-04 18:14:49 -0300

    feat(details): improve details header

--- app/details/[symbol]/page.tsx
+++ app/details/[symbol]/page.tsx
@@ -1,9 +1,34 @@
 'use client'
 
-import { useParams } from 'next/navigation'
+import { useParams, useRouter } from 'next/navigation'
+import { SectionHeader } from '@/components/ui/section-header'
+import { ArrowLeft } from 'lucide-react'
 
 export default function DetailsPage() {
   const params = useParams()
+  const router = useRouter()
 
-  return <div className='container mx-auto p-6'>{`Detalhes ${params}`}</div>
+  const handleBack = () => {
+    router.back()
+  }
+
+  return (
+    <div className='container mx-auto p-6'>
+      <div className='flex items-start gap-4 '>
+        <button
+          onClick={handleBack}
+          className='mt-4 text-primary hover:text-primary/70 transition-colors cursor-pointer'
+          aria-label='Voltar para página anterior pointer'
+        >
+          <ArrowLeft size={24} />
+        </button>
+        <SectionHeader
+          title='Detalhes'
+          subtitle={`Informações detalhadas sobre ${decodeURIComponent(
+            params.symbol as string
+          )}`}
+        />
+      </div>
+    </div>
+  )
 }
