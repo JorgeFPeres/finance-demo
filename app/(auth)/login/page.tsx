e7e472854325e5b43cc5bc93f9c2be3068eb49ab
JorgeFPeres <jorge-peres@live.com>
2025-04-04 17:04:15 -0300

    feat(layout): improve layout and add logo

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -2,8 +2,8 @@ import { AuthForm } from '@/components/shared/AuthForm'
 
 export default function LoginPage() {
   return (
-    <div className='flex min-h-screen items-center justify-center bg-gray-50 p-4'>
-      <div className='bg-white shadow-xl rounded-xl p-8 w-full max-w-md'>
+    <div className='flex-1 flex items-center justify-center p-4'>
+      <div className='w-full max-w-md bg-card border rounded-xl p-8 shadow-lg'>
         <AuthForm />
       </div>
     </div>
